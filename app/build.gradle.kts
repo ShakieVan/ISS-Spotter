dd7a9d3d19752d366337d9db801c6a39c63be943
ShakieVan <55868847+ShakieVan@users.noreply.github.com>
2026-09-19 12:03:50 +0200

    chore: Bump version to 1.0.25 (Code 26)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "de.shakie.iss"
         minSdk = 28
         targetSdk = 36
-        versionCode = 25
-        versionName = "1.0.24"
+        versionCode = 26
+        versionName = "1.0.25"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -51,7 +51,7 @@ android {
 }
 
 base {
-    archivesName.set("ISS-Spotter-v1.0.24")
+    archivesName.set("ISS-Spotter-v1.0.25")
 }
 
 dependencies {
